d4958dc2c2649f81421758b9baf844229ae08cd8
Me <fox.derrick@gmail.com>
2020-03-17 13:24:32 -0400

    created upload button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { UploadComponentComponent } from './upload-component/upload-component.component';
+import { FileUploadModule } from 'primeng/fileupload';
+import { MessageModule } from 'primeng/message';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { UploadComponentComponent } from './upload-component/upload-component.co
     UploadComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FileUploadModule,
+    MessageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/upload-component/upload-component.component.ts
+++ src/app/upload-component/upload-component.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { AppComponent } from './../app.component';
 
 @Component({
   selector: 'app-upload-component',
+  providers: [MessageService],
   templateUrl: './upload-component.component.html',
   styleUrls: ['./upload-component.component.css']
 })
-export class UploadComponentComponent implements OnInit {
+export class UploadComponentComponent {
 
-  constructor() { }
+  uploadedFiles: any[] = [];
 
-  ngOnInit() {
+  constructor(private messageService: MessageService, private app: AppComponent) { }
+
+  onUpload(event) {
+    for (let file of event.files) {
+      this.uploadedFiles.push(file);
+    }
+
+    this.messageService.add({ severity: 'info', summary: 'Success', detail: 'File Uploaded' });
   }
+
+  onBasicUpload(event) {
+    this.messageService.add({ severity: 'info', summary: 'Success', detail: 'File Uploaded with Basic Mode' });
+  }
+
+  onBasicUploadAuto(event) {
+    this.messageService.add({ severity: 'info', summary: 'Success', detail: 'File Uploaded with Auto Mode' });
+  }
+
+  // isNewsActive() {
+  //     return this.app.newsActive;
+  // }
 
 }

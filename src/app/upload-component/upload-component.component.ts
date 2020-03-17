84b6474c6cfaac537dedc7418705c16828aad9d4
Me <fox.derrick@gmail.com>
2020-03-17 12:16:33 -0400

    created upload component

---
+++ src/app/upload-component/upload-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-component',
+  templateUrl: './upload-component.component.html',
+  styleUrls: ['./upload-component.component.css']
+})
+export class UploadComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
